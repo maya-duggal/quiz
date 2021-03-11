2b0f002037af480362b5331d57d611e9c25a9f73
maya-duggal <79423750+maya-duggal@users.noreply.github.com>
2021-03-11 19:35:00 +0000

    Add files via upload

---
+++ question.js
@@ -0,0 +1,15 @@
+class Question{
+display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350,0);
+
+    this.question.html("question- what starts and ends with the letter 'e', but only has one letter?")
+    this.question.position(150, 80)
+    this.option1.html("1-everyone")
+    this.option1.position(150, 100)
+    this.option2.html("2-envelope")
+    this.option2.position(150, 120)
+    
+this.input1.position(150,120);
+}
+}
